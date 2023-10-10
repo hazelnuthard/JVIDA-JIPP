db78de352f1146e208cc50f552c558b73d79c08d
hazelnuthard <142935760+hazelnuthard@users.noreply.github.com>
2023-10-10 18:24:52 -0300

    Update JVIDA-JIPP_View.h

--- JVIDA-JIPP_View.h
+++ JVIDA-JIPP_View.h
@@ -6,11 +6,11 @@ Pedro Evaristo de Oliveira
 Priscilla de Souza Jardim
 */
 
-int dimensao();
-int menu_geral();
-int coord_x();
-int coord_y();
-char exclui(int, int);
-void apresenta_mundo(char, int);
-void resp_invalida();
-void limpatela();
+int dimensao();  //recebe a dimensao do mundo digitada pelo usuario
+int menu_geral();  //apresenta o menu de opcoes das acoes do jogo
+int coord_x();  //recebe a linha de onde o usuario deseja inserir a celula
+int coord_y();  //recebe a coluna de onde o usuario deseja inserir a celula
+char exclui(int, int);  //confirma se o usuario deseja excluir a celula
+void apresenta_mundo(char, int);  //apresenta a matriz do mundo
+void resp_invalida();  //mensagem padrao de resposta indevida do usuario
+void limpatela();  //organiza melhor o codigo removendo acoes anteriores da tela
